5bf14191cd5b501bebdd706189c94d5b3765abe9
Attreyee Mukherjee <86184935+a-muk@users.noreply.github.com>
2022-01-02 11:45:09 +0530

    new update

--- covidclickapp/settings.py
+++ covidclickapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7l!y$isy#!kauu!#4#gk1@jyo@148=y!9qndx+dyi_)snmn%@d'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +42,14 @@ INSTALLED_APPS = [
     'Hackathon',
     'django.contrib.postgres',
 ]
+
+THIRD_PARTY_APPS=[
+
+]
+
+LOCAL_APPS=[]
+
+INSTALLED_APPS= DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -77,18 +85,11 @@ WSGI_APPLICATION = 'covidclickapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'isemiriu',
-        'USER': 'isemiriu',
-        'PASSWORD': 'dE5EiWZaOHNNwPCfS1xuIo512TRLGRrh',
-        'HOST':'fanny.db.elephantsql.com'
-    }
+
 
         
     
-}
+
 
 
 # Password validation
@@ -127,17 +128,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),r"C:\Users\covidclickapp\projects\covidclickapp\static",]
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'covidclick2021@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hackathon@2021'
+
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from covidclickapp.local_settings import *
+except ImportError:
+    pass
